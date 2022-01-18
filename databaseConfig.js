9e150ef54337a4a7e908ff17913cabf40d7fcfcb
Rae <RaeTStone@gmail.com>
2022-01-18 17:53:38 -0500

    fixed cached files

--- databaseConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST: 'ec2-3-230-61-252.compute-1.amazonaws.com',
-    USER: 'xnfvusapnozfoj',
-    PASSWORD: 'b124596c52b7deb53c919ccf2d7edbfb4ef8422bd9b6e79af147bb9a61dd8753',
-    DB: 'd6gkjgnck88lpi',
-    dialect: 'postgres'
-}
