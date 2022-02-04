42b44f81c52564f1fa6cf00e0d44f31e83b24167
Rae <RaeTStone@gmail.com>
2022-02-04 15:58:31 -0500

    fixed full route

--- controllers/iraController.js
+++ controllers/iraController.js
@@ -82,27 +82,29 @@ const getIraFull = async (req, res) => {
         if (ira){
             let codes = "";
             let investments = ira.dataValues.investments;
-            let len = 1;
-            for (const investment of investments){
-                codes = codes + investment.dataValues.name + ",";
-                len++;
-            }
-            codes = codes.substring(0, codes.length -1);
-            requestify.get('https://boiling-falls-79972.herokuapp.com/current/' + codes)
-            .then((response) => {
-            let stocks = response.getBody();
-            let prices = [];
-            stocks.stocks.forEach((stock) => {
-                prices.push(stock.stock_value);
+            if (investments.length > 0){
+                let len = 1;
+                for (const investment of investments){
+                    codes = codes + investment.dataValues.name + ",";
+                    len++;
+                }
+                codes = codes.substring(0, codes.length -1);
+                requestify.get('https://boiling-falls-79972.herokuapp.com/current/' + codes)
+                .then((response) => {
+                let stocks = response.getBody();
+                let prices = [];
+                stocks.stocks.forEach((stock) => {
+                    prices.push(stock.stock_value);
+                })
+                let i = 0;
+                investments.forEach((inv) => {
+                    inv.dataValues.currentValue = prices[i];
+                    i++;
+                })
             })
-            let i = 0;
-            investments.forEach((inv) => {
-                inv.dataValues.currentValue = prices[i];
-                i++;
-            })
-            res.status(200).send(ira);
-        })
         }
+        res.status(200).send(ira);
+    }
         else {
             res.status(200).send({});
         }
